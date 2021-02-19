98657ced36113e8a27d0b19e812de70c09502a0e
jiaming3 <60019124+jiaming3@users.noreply.github.com>
2021-02-19 17:18:47 +0000

    Update motor.py

--- comb/nxp1/motor.py
+++ comb/nxp1/motor.py
@@ -1,171 +1,48 @@
-import RPi.GPIO as GPIO
+import RPi.GPIO as gpio
 import time
 
-out1 = 13
-out2 = 11
-out3 = 15
-out4 = 12
 
-i = 0
-positive = 0
-negative = 0
-y = 0
-
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(out1, GPIO.OUT)
-GPIO.setup(out2, GPIO.OUT)
-GPIO.setup(out3, GPIO.OUT)
-GPIO.setup(out4, GPIO.OUT)
-
-print("First calibrate by giving some +ve and -ve values.....")
-GPIO.output(out1, GPIO.LOW)
-GPIO.output(out2, GPIO.LOW)
-GPIO.output(out3, GPIO.LOW)
-GPIO.output(out4, GPIO.LOW)
-x = input()
-if x > 0 and x <= 400:
-    for y in range(x, 0, -1):
-        if negative == 1:
-            if i == 7:
-                i = 0
-            else:
-                i = i + 1
-            y = y + 2
-            negative = 0
-        positive = 1
-        # print((x+1)-y)
-        if i == 0:
-            GPIO.output(out1, GPIO.HIGH)
-            GPIO.output(out2, GPIO.LOW)
-            GPIO.output(out3, GPIO.LOW)
-            GPIO.output(out4, GPIO.LOW)
-            time.sleep(0.03)
-            # time.sleep(1)
-        elif i == 1:
-            GPIO.output(out1, GPIO.HIGH)
-            GPIO.output(out2, GPIO.HIGH)
-            GPIO.output(out3, GPIO.LOW)
-            GPIO.output(out4, GPIO.LOW)
-            time.sleep(0.03)
-            # time.sleep(1)
-        elif i == 2:
-            GPIO.output(out1, GPIO.LOW)
-            GPIO.output(out2, GPIO.HIGH)
-            GPIO.output(out3, GPIO.LOW)
-            GPIO.output(out4, GPIO.LOW)
-            time.sleep(0.03)
-            # time.sleep(1)
-        elif i == 3:
-            GPIO.output(out1, GPIO.LOW)
-            GPIO.output(out2, GPIO.HIGH)
-            GPIO.output(out3, GPIO.HIGH)
-            GPIO.output(out4, GPIO.LOW)
-            time.sleep(0.03)
-            # time.sleep(1)
-        elif i == 4:
-            GPIO.output(out1, GPIO.LOW)
-            GPIO.output(out2, GPIO.LOW)
-            GPIO.output(out3, GPIO.HIGH)
-            GPIO.output(out4, GPIO.LOW)
-            time.sleep(0.03)
-            # time.sleep(1)
-        elif i == 5:
-            GPIO.output(out1, GPIO.LOW)
-            GPIO.output(out2, GPIO.LOW)
-            GPIO.output(out3, GPIO.HIGH)
-            GPIO.output(out4, GPIO.HIGH)
-            time.sleep(0.03)
-            # time.sleep(1)
-        elif i == 6:
-            GPIO.output(out1, GPIO.LOW)
-            GPIO.output(out2, GPIO.LOW)
-            GPIO.output(out3, GPIO.LOW)
-            GPIO.output(out4, GPIO.HIGH)
-            time.sleep(0.03)
-            # time.sleep(1)
-        elif i == 7:
-            GPIO.output(out1, GPIO.HIGH)
-            GPIO.output(out2, GPIO.LOW)
-            GPIO.output(out3, GPIO.LOW)
-            GPIO.output(out4, GPIO.HIGH)
-            time.sleep(0.03)
-            # time.sleep(1)
-        if i == 7:
-            i = 0
-            continue
-        i = i + 1
+def init():
+    gpio.setmode(gpio.BCM)
+    gpio.setup(17, gpio.OUT)
+    gpio.setup(22, gpio.OUT)
+    gpio.setup(23, gpio.OUT)
+    gpio.setup(24, gpio.OUT)
 
 
-elif x < 0 and x >= -400:
-    x = x * -1
-    for y in range(x, 0, -1):
-        if positive == 1:
-            if i == 0:
-                i = 7
-            else:
-                i = i - 1
-            y = y + 3
-            positive = 0
-        negative = 1
-        # print((x+1)-y)
-        if i == 0:
-            GPIO.output(out1, GPIO.HIGH)
-            GPIO.output(out2, GPIO.LOW)
-            GPIO.output(out3, GPIO.LOW)
-            GPIO.output(out4, GPIO.LOW)
-            time.sleep(0.03)
-            # time.sleep(1)
-        elif i == 1:
-            GPIO.output(out1, GPIO.HIGH)
-            GPIO.output(out2, GPIO.HIGH)
-            GPIO.output(out3, GPIO.LOW)
-            GPIO.output(out4, GPIO.LOW)
-            time.sleep(0.03)
-            # time.sleep(1)
-        elif i == 2:
-            GPIO.output(out1, GPIO.LOW)
-            GPIO.output(out2, GPIO.HIGH)
-            GPIO.output(out3, GPIO.LOW)
-            GPIO.output(out4, GPIO.LOW)
-            time.sleep(0.03)
-            # time.sleep(1)
-        elif i == 3:
-            GPIO.output(out1, GPIO.LOW)
-            GPIO.output(out2, GPIO.HIGH)
-            GPIO.output(out3, GPIO.HIGH)
-            GPIO.output(out4, GPIO.LOW)
-            time.sleep(0.03)
-            # time.sleep(1)
-        elif i == 4:
-            GPIO.output(out1, GPIO.LOW)
-            GPIO.output(out2, GPIO.LOW)
-            GPIO.output(out3, GPIO.HIGH)
-            GPIO.output(out4, GPIO.LOW)
-            time.sleep(0.03)
-            # time.sleep(1)
-        elif i == 5:
-            GPIO.output(out1, GPIO.LOW)
-            GPIO.output(out2, GPIO.LOW)
-            GPIO.output(out3, GPIO.HIGH)
-            GPIO.output(out4, GPIO.HIGH)
-            time.sleep(0.03)
-            # time.sleep(1)
-        elif i == 6:
-            GPIO.output(out1, GPIO.LOW)
-            GPIO.output(out2, GPIO.LOW)
-            GPIO.output(out3, GPIO.LOW)
-            GPIO.output(out4, GPIO.HIGH)
-            time.sleep(0.03)
-            # time.sleep(1)
-        elif i == 7:
-            GPIO.output(out1, GPIO.HIGH)
-            GPIO.output(out2, GPIO.LOW)
-            GPIO.output(out3, GPIO.LOW)
-            GPIO.output(out4, GPIO.HIGH)
-            time.sleep(0.03)
-            # time.sleep(1)
-        if i == 0:
-            i = 7
-            continue
-        i = i - 1
+def forward(tf):
+    init()
+    gpio.output(17, True)
+    gpio.output(22, False)
+    gpio.output(23, True)
+    gpio.output(24, False)
+    time.sleep(tf)
+    gpio.cleanup()
 
+
+def reverse(tf):
+    init()
+    gpio.output(17, False)
+    gpio.output(22, True)
+    gpio.output(23, False)
+    gpio.output(24, True)
+    time.sleep(tf)
+    gpio.cleanup()
+
+
+def stop(tf):
+    init()
+    gpio.output(17, False)
+    gpio.output(22, False)
+    gpio.output(23, False)
+    gpio.output(24, False)
+    time.sleep(tf)
+    gpio.cleanup()
+
+#while True:
+ #   forward(1.5)
+  #  reverse(0.025)
+   # stop(2)
+    #reverse(1.5)
+    #forward(0.025)
+    #stop(2)
