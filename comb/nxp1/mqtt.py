b7561f4d62ddb66ba61ae0f8a1f20ec2fb0de425
dsh-ic <79240156+dsh-ic@users.noreply.github.com>
2021-02-19 16:09:10 +0800

    Update mqtt.py

--- comb/nxp1/mqtt.py
+++ comb/nxp1/mqtt.py
@@ -4,14 +4,16 @@ import paho.mqtt.client as mqtt
 import json
 import time
 receive_data = 1
-client = mqtt.Client(userdata=receive_data)
-client.connect("test.mosquitto.org",port=1883)
-def on_message(client, userdata, message):
+
+def ini_mqtt():
+ client = mqtt.Client(userdata=receive_data)
+ client.connect("test.mosquitto.org",port=1883)
+ def on_message(client, userdata, message):
    userdata = json.loads(message.payload)
    client.user_data_set(userdata)
-   
-client.on_message = on_message
-client.subscribe("IC.embedded/Team_ALG/#")
+ client.on_message = on_message()
+ client.subscribe("IC.embedded/Team_ALG/#")
+ 
 
 def publish_data(temp,hum,pressure,flower_pot):
   payload=json.dumps({"temp":temp,"humidity":hum,"pressure":pressure,"flowerpot":flower_pot})
