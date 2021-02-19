c72bf9a1c32020100ed9993db0cca2c3b22850f7
dsh-ic <79240156+dsh-ic@users.noreply.github.com>
2021-02-19 15:53:15 +0800

    Update mqtt.py

--- comb/nxp1/mqtt.py
+++ comb/nxp1/mqtt.py
@@ -3,13 +3,13 @@ import paho.mqtt.client as mqtt
 
 import json
 import time
-
-client = mqtt.Client()
+receive_data = 1
+client = mqtt.Client(userdata=receive_data)
 client.connect("test.mosquitto.org",port=1883)
 def on_message(client, userdata, message):
-   print("Received message:{} on topic{}".format(message.payload, message.topic))
-   data = json.loads(message.payload)
-   return data
+   userdata = json.loads(message.payload)
+   client.user_data_set(userdata)
+   
 client.on_message = on_message
 client.subscribe("IC.embedded/Team_ALG/#")
 
@@ -18,9 +18,12 @@ def publish_data(temp,hum,pressure,flower_pot):
   client.publish("IC.embedded/Team_ALG/test",payload)
   #default keep alive is 60s,thus we need at least 1 imformation change between broker and client per minutes#
   
-#def receive():
-  #data = client.on_message()
-  # do we need pi to receive any message from app??
+def receive():
+  client.loop_start()
+  time.sleep(20) #change holding-connection-state time here
+  client.loop_stop()
+  return receive_data
+   
   
   
   
